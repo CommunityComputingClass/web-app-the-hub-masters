ae9a5113e3d28748298f36913d9a5546dbdc0adb
asianjesusfr <157668714+asianjesusfr@users.noreply.github.com>
2025-03-19 17:01:33 +0000

    pulsing text

--- project/bets.js
+++ project/bets.js
@@ -1,11 +1,29 @@
+
+let sizeVar = 20
+let sizeChange = 1
 function setup() {
     createCanvas(400, 400);
-    background(100, 68, 227);
   }
-  
-  function draw() {
-    strokeWeight(2);
-    fill(255)
-    textSize(12 + (mouseX / width)*72);
-    text("Attention, please.", 50, 200);
-  }
+
+function draw() {
+  background(100, 68, 227);
+  fill(255)
+  strokeWeight(5);
+    textAlign(CENTER)
+
+   
+   
+    textSize(sizeVar)
+    sizeVar = sizeVar + sizeChange
+    
+    if(sizeVar == 120){
+      sizeChange = -1
+    }
+
+    
+    
+  //     for (let i = 0; i<10; i = i+1) {
+  //       textSize(10 + i);
+  // }
+    text("Bets!", 200, 200);
+}
